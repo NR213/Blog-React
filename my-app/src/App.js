f43714ec6d3fa95daa4e5830672ddd363bf63a18
Nishanth Raikar <nishanth.raikar213@gmail.com>
2023-03-31 16:41:13 +0200

    initial commit

---
+++ my-app/src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+import Header from './navbar.js';
+function App() {
+  return (
+    <main>
+      <Header />
+   </main>
+    
+  );
+}
+
+export default App;
---
+++ my-app/src/navbar.js
@@ -0,0 +1,11 @@
+export default function Header() {
+    return (
+        <div>
+            <a href="" className = "blog">Blog</a>
+            <nav>
+            <a href="">Login</a>
+            <a href="">Register</a>
+            </nav>
+        </div>
+    );
+}
